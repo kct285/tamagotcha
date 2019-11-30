8747010770a10fae2c033adddd62230373b4f3e1
kct285 <kimberlytanchay@Kimberlys-MacBook-Pro.local>
2019-11-30 19:24:32 +0800

    halp halp

--- src/App.js
+++ src/App.js
@@ -1,19 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
+// import Modal from './components/Modal';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+
+      {/* <Modal /> */}
+
+
+
+
+      <button className="infobutton">?</button>
+
+      
+      <div className="infomodal">
+        <div className="info_modalcontent">
+        <span className="close"> &times;</span>
+        <h3 className="infomessage"> Nice to meet you!</h3>
+        <p> It's simple. Earn money to keep me happy and healthy. <br></br> <br></br>
+        Spend too much and I'll get sick and die :( </p>
+        </div>
+      </div>
+
+
       <img id="heartbar" src= {process.env.PUBLIC_URL + '/3hearts.png'} /> 
       <img id="baby" src={process.env.PUBLIC_URL + '/baby.gif'} />
 
 
-      <div class="button_flex">
-      <div class="button">
+      <div className="button_flex">
+      <div className="button">
         <a href="#"> + </a>
       </div>
-      <div class="button">
+      <div className="button">
         <a href="#"> - </a>
       </div>
 
@@ -25,11 +44,9 @@ function App() {
 
 
 
-
-
-
     </div>
   );
 }
+
 
 export default App;
