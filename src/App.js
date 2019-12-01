9a84a5bf330b1e006a4392322a25a14d19fb02a4
kct285 <kimberlytanchay@Kimberlys-MacBook-Pro.local>
2019-12-01 08:56:38 +0800

    black box added

--- src/App.js
+++ src/App.js
@@ -19,7 +19,9 @@ class App extends Component {
         <span className="close" onClick={(e) => this.setState ({isOpen:false })}> &times;</span>
         <h3 className="infomessage"> Nice to meet you!</h3>
         <p> It's simple. Earn money to keep me happy and healthy. <br></br> <br></br>
-        Spend too much and I'll get sick and die :( </p>
+        Spend too much and I'll get sick and die :( <br></br> <br></br>
+        Click the "+" to add earned money <br></br> and "-" for spend money
+           </p>
         </div>
       </div>
 
@@ -27,11 +29,12 @@ class App extends Component {
 
       <button className="infobutton" onClick={(e) => this.setState ({isOpen:true })}> ?</button>
 
-      
-      
-
 
       <img id="heartbar" src= {process.env.PUBLIC_URL + '/3hearts.png'} /> 
+
+      <div id="money"> [number] </div>
+
+
       <img id="baby" src={process.env.PUBLIC_URL + '/baby.gif'} />
 
 
